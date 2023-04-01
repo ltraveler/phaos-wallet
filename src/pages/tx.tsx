2eb360ce950a819c8b216ad129640edac72f8a3a
LTraveler <ltraveler@protonmail.com>
2023-04-01 16:10:56 -0500

    The Transaction Status row has been added to the transaction page

--- src/pages/tx.tsx
+++ src/pages/tx.tsx
@@ -129,8 +129,16 @@ const Tx = () => {
                 <dl>
                   <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                     <dt className="text-sm font-medium text-gray-500">TX</dt>
-                    <dd className="mt-1 overflow-x-auto text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                    <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
                       {tx}
+                    </dd>
+                  </div>
+                  <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt className="text-sm font-medium text-gray-500">
+                      Transaction Status
+                    </dt>
+                    <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                      {status}
                     </dd>
                   </div>
                 </dl>
