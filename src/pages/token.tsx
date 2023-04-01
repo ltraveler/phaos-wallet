fb01cb3e83e9bcfc3c90ec5d1524fa21a96fcb2c
LTraveler <ltraveler@protonmail.com>
2023-03-31 21:46:48 -0500

    minor changes related to the previous commit

--- src/pages/token.tsx
+++ src/pages/token.tsx
@@ -203,7 +203,7 @@ const AuthorProfilePage: NextPageWithLayout<
                   </Button>
                 ) : (
                   <div>
-                    <div className="mt-6 px-4 font-bold text-gray-500 dark:text-gray-400 sm:px-6 lg:mx-auto lg:px-8">
+                    <div className="mt-6 px-4 font-bold text-gray-500 dark:text-gray-400 sm:px-6">
                       <p>❄ Age must be above 6 to claim</p>
                     </div>
                     <Button
